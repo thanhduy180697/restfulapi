e6ff8d9676dc909442c421eff25e4db2337dff51
Doan-Thanh-Duy <thanhduy180697@gmail.com>
2020-03-12 18:13:14 +0700

    ABC

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,11 @@ use Illuminate\Http\Request;
 */
 
 Route::resource('buyers', 'Buyer\BuyerController',['only' => ['index','show']]);
+Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController',['only' => ['index']]);
+Route::resource('buyers.products', 'Buyer\BuyerProductController',['only' => ['index']]);
+Route::resource('buyers.sellers', 'Buyer\BuyerSellerController',['only' => ['index']]);
+Route::resource('buyers.categories', 'Buyer\BuyerCategoryController',['only' => ['index']]);
+
 
 
 /*
@@ -25,8 +30,9 @@ Route::resource('buyers', 'Buyer\BuyerController',['only' => ['index','show']]);
 */
 
 Route::resource('categories', 'Category\CategoryController',['except' => ['create','edit']]);
-
-
+Route::resource('categories.products', 'Category\CategoryProductController',['only' => ['index']]);
+Route::resource('categories.sellers', 'Category\CategorySellerController',['only' => ['index']]);
+Route::resource('categories.transactions', 'Category\CategoryTransactionController',['only' => ['index']]);
 /*
 *	Product
 */
@@ -46,7 +52,9 @@ Route::resource('sellers', 'Seller\SellerController',['only' => ['index','show']
 *	Transaction
 */
 
-Route::resource('transactions', 'Transaction\TransactionController',['only' => ['index','show']]);
+Route::resource('transactions', 'Transaction\TransactionController',['only'=> ['index','show']]);
+Route::resource('transactions.categories', 'Transaction\TransactionCategoryController',['only' => ['index']]);
+Route::resource('transactions.sellers', 'Transaction\TransactionSellerController',['only' => ['index']]);
 
 /*
 *	User
